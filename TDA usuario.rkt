5ca9c059c09e9bcbf972fbd9384477a11e979ca6
Nicolas <nicolas.venegas.a@usach.cl>
2021-10-23 22:28:30 -0300

    Creacion TDA usuario

---
+++ TDA usuario.rkt
@@ -0,0 +1,42 @@
+#lang racket
+; Se definira el tda usuario con los siguientes elementos
+; (nombre, contraseña)
+; se tomara en consideracion que los usuarios siempren se almacenaran en listas para funciones de busqueda
+
+; Constructor de un usuario
+; Dom: StringXString
+; Rec: usuario
+(define (usuario user cont) (
+                             list user cont))
+
+; obtencion del nombre de un usuario
+; Dom: usuario
+; Rec: String
+(define (getUser usuario) (car usuario))
+
+; obtencion de la contraseña de un usuario
+; Dom: usuario
+; Rec: String
+(define (getPass usuario) (cdr usuario))
+
+; comprobacion de si es un usuario o no
+; Dom: SupuestoUser
+; Rec: Boolean
+(define (user? usuario) (
+                         if (= (length usuario )2)
+                            (andmap string? usuario) #f))
+
+; verificar si el nombre de usuario es igual un string
+; Dom: usuarioXString
+; Rec: Boolean
+(define (compNombre usuario texto)(= texto (car usuario)))
+
+; Comprobar si un user y un password entregado concide con el usuario
+; Dom: usuarioXStringXString
+; Rec: Boolean
+(define (userLog usuario user pass) (and
+                                     (= user (car usuario) )
+                                     (= pass (cdr usuario))))
+
+
+
